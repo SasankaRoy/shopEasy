792426b81d3ed21c8ccdd4e6e6ee8152df0f1ad8
SasankaRoy <104346053+SasankaRoy@users.noreply.github.com>
2023-08-18 11:28:06 +0530

    solved

--- pages/admin/dashboard.jsx
+++ pages/admin/dashboard.jsx
@@ -20,17 +20,15 @@ const Dashboard = () => {
         <NavBar />
         <div className="h-[93.5vh] flex justify-center items-center space-x-1 overflow-hidden">
           <div className="side h-full w-auto p-2 md:w-[15%] hidden md:inline    sticky  top-0  overflow-hidden">
-            <Sidebar />
+            {/* <Sidebar /> */}
           </div>
           <div className="main  flex-1 h-full  overflow-x-hidden overflow-y-scroll scroll-smooth">
-            <div className="boxs">
-              <Boxes />
-            </div>
+            <div className="boxs">{/* <Boxes /> */}</div>
             <div className="rounded-xl m-2  md:m-5 ">
-              <Charts />
+              {/* <Charts /> */}
               <div className="flex flex-col md:flex-row mt-8">
-                <LeftWidget />
-                <RightWidget />
+                {/* <LeftWidget />
+                <RightWidget /> */}
               </div>
             </div>
             <div className="flex-1 p-5">
