9743f8862bc5925c73da47d7c26ef60d1a8ba31b
SasankaRoy <104346053+SasankaRoy@users.noreply.github.com>
2023-09-30 22:37:29 +0530

    env file updated

--- next.config.js
+++ next.config.js
@@ -5,18 +5,18 @@ const nextConfig = {
   images: {
     domains: ["res.cloudinary.com"],
   },
-  env: {
-    MONGO_URI:
-      "mongodb+srv://babu-E-comm:McHoTA00JN6goxAr@e-comm.ydg5kjg.mongodb.net/?retryWrites=true&w=majority",
-    SECRET_KEY:
-      "THESECRETISVERYSECRETandITCANNOTbeSHAREDwithOTHER-_2PERSONS__ORBEpublic",
-    MEDIA_TO_MEDIAURL_CONVERTER:
-      "https://api.cloudinary.com/v1_1/dcgmbgmyk/image/upload",
-    CLOUD_NAME: "dcgmbgmyk",
-    UPLOAD_PRESET: "e-comm",
-    PRODUCTION_DOMAIN: "https://shop-easee.vercel.app",
-    DEVELOPMENT_DOMAIN: "http://localhost:3000",
-  },
+  // env: {
+  //   MONGO_URI:
+  //     "mongodb+srv://babu-E-comm:McHoTA00JN6goxAr@e-comm.ydg5kjg.mongodb.net/?retryWrites=true&w=majority",
+  //   SECRET_KEY:
+  //     "THESECRETISVERYSECRETandITCANNOTbeSHAREDwithOTHER-_2PERSONS__ORBEpublic",
+  //   MEDIA_TO_MEDIAURL_CONVERTER:
+  //     "https://api.cloudinary.com/v1_1/dcgmbgmyk/image/upload",
+  //   CLOUD_NAME: "dcgmbgmyk",
+  //   UPLOAD_PRESET: "e-comm",
+  //   PRODUCTION_DOMAIN: "https://shop-easee.vercel.app",
+  //   DEVELOPMENT_DOMAIN: "http://localhost:3000",
+  // },
 };
 // passwords = McHoTA00JN6goxAr mongodb.
 module.exports = nextConfig;
