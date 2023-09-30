c0d4f7a523f943f81d0db925b660071aa4406541
SasankaRoy <104346053+SasankaRoy@users.noreply.github.com>
2023-09-30 23:30:01 +0530

    error fixed

--- utils/connectDb.js
+++ utils/connectDb.js
@@ -7,7 +7,7 @@ const connectDB = async () => {
     return;
   }
   try {
-    await mongoose.connect(process.env.MONGO_URI, {
+    await mongoose.connect(process.env.NEXT_PUBLIC_MONGO_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
