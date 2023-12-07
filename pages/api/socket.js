58847efbcb269c79fdc4b997c97ee4f89b1d9266
SasankaRoy <104346053+SasankaRoy@users.noreply.github.com>
2023-12-07 23:58:39 +0530

    socket error solving

--- pages/api/socket.js
+++ pages/api/socket.js
@@ -4,28 +4,29 @@ import socketMiddleware from "../../utils/socketMiddleware";
 
 const CORS = socketMiddleware(
   Cors({
-    methods:['GET', 'POST', 'OPTIONS']
+    methods: ['GET', 'POST', 'OPTIONS'],
+    origin: ['https://velvety-babka-72c4ff.netlify.app/', 'http://localhost:3000/', 'https://shop-easee.vercel.app/']
   })
-) 
+)
 
 // Create a shared io instance outside the handler
 const io = new Server();
 
 export default async function handler(req, res) {
   try {
-      await CORS(req, res);
-      const io = new Server(res.socket.server);
-      res.socket.server.io = io;
+    await CORS(req, res);
+    const io = new Server(res.socket.server);
+    res.socket.server.io = io;
 
-      io.on("connection", (socket) => {
-        console.log("Connection");
-        socket.on("test__SocketServer", (data) => {
-            io.emit("test__SocketServer", data);            
-        });
+    io.on("connection", (socket) => {
+      console.log("Connection");
+      socket.on("test__SocketServer", (data) => {
+        io.emit("test__SocketServer", data);
       });
+    });
 
 
-    
+
 
     console.log("hello");
 
