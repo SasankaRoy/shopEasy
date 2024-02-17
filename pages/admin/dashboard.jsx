3a198110964cdc09b97e7bbe9a9d808565538b2d
SasankaRoy <104346053+SasankaRoy@users.noreply.github.com>
2024-02-17 23:58:23 +0530

    dashboard update

--- pages/admin/dashboard.jsx
+++ pages/admin/dashboard.jsx
@@ -125,36 +125,36 @@ const Dashboard = () => {
     fetchOders();
   }, [updater]);
 
-  const stringToColor = (string) => {
-    let hash = 0;
-    let i;
+  // const stringToColor = (string) => {
+  //   let hash = 0;
+  //   let i;
 
-    /* eslint-disable no-bitwise */
-    for (i = 0; i < string.length; i += 1) {
-      hash = string.charCodeAt(i) + ((hash << 5) - hash);
-    }
+  //   /* eslint-disable no-bitwise */
+  //   for (i = 0; i < string.length; i += 1) {
+  //     hash = string.charCodeAt(i) + ((hash << 5) - hash);
+  //   }
 
-    let color = "#";
+  //   let color = "#";
 
-    for (i = 0; i < 3; i += 1) {
-      const value = (hash >> (i * 8)) & 0xff;
-      color += `00${value.toString(16)}`.slice(-2);
-    }
-    /* eslint-enable no-bitwise */
+  //   for (i = 0; i < 3; i += 1) {
+  //     const value = (hash >> (i * 8)) & 0xff;
+  //     color += `00${value.toString(16)}`.slice(-2);
+  //   }
+  //   /* eslint-enable no-bitwise */
 
-    return color;
-  };
+  //   return color;
+  // };
 
-  const stringAvatar = (name) => {
-    return {
-      sx: {
-        bgcolor: stringToColor(name),
-        fontWeight: 500,
-        fontSize: 18,
-      },
-      children: `${name.split(" ")[0][0]}${name.split(" ")[1][0]}`,
-    };
-  };
+  // const stringAvatar = (name) => {
+  //   return {
+  //     sx: {
+  //       bgcolor: stringToColor(name),
+  //       fontWeight: 500,
+  //       fontSize: 18,
+  //     },
+  //     children: `${name.split(" ")[0][0]}${name.split(" ")[1][0]}`,
+  //   };
+  // };
 
   return (
     <>
@@ -217,7 +217,7 @@ const Dashboard = () => {
                   >
                     <td className="font-semibold text-center text-md tracking-wider p-2 flex justify-start items-center space-x-2">
                       <Avatar
-                        {...stringAvatar(cur.userName)}
+                        // {...stringAvatar(cur.userName)}
                         round={true}
                         size={40}
                       ></Avatar>
